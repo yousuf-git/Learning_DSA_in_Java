7983eec86d7461536159174053b2f064d4aff43d
yousuf-git <hackerbest052@gmail.com>
2024-06-21 16:33:56 +0500

    Added Shell Sort and updated time complexities

--- 04-Lec-16(Sorting)/Sorting.java
+++ 04-Lec-16(Sorting)/Sorting.java
@@ -1,6 +1,7 @@
 public class Sorting {
     public static void main(String[] args) {
         // Bubble sort (in each iteration if previous is greater than next one, swap them)
+        // Time Complexity: 𝑂(𝑛²)
         int[] array = { 5, 4, 3, 6, 7, -1 };
         // Use an iterator (i) from 0 to len - 1 bcz last element is already sorted
         for (int i = 0; i < array.length - 1; i++) {
@@ -19,6 +20,7 @@ public class Sorting {
         printArray(array); // a user-dfined method to print the elements of array
 
         // Selection sort: Find smallest element and swap with 1st unsorted element
+        // Time Complexity: 𝑂(𝑛²)
         int marks[] = { 50, 90, 40, 30, 69 }; // user input can also be used
         int minIdx, swap; // minIdx will track the index of smallest element in each iteration
         for (int i = 0; i < marks.length - 1; i++) {
@@ -36,17 +38,21 @@ public class Sorting {
         System.out.print("Sorted Marks by Selection sort: ");
         printArray(marks);
 
-        // insertion sort
+        /* Insertion sort 
+         * Time Complexity: 𝑂(𝑛²) in the average and worst case, but 𝑂(𝑛) in the best case for nearly sorted data
+         * Divide the array into 2 parts, sorted and unsorted
+         * Choose 1 element from the unsorted portion and place it to its correct position in sorted portion
+        */
         int[] nums = { 2, 543, 453, 54, 453, 4, 342 };
-        // i for unsorted array, j for sorted part, (assuming index 0 is sorted)
+        // i for unsorted part, j for sorted part, (assuming index 0 is sorted initially)
         for (int i = 1; i < nums.length; i++) {
-            int element = nums[i];
+            int element = nums[i]; // store the current element because its index will be used to shift elements from unsorted portion
             // Make place for element in sorted array
             int j = i - 1; // sorted array ends just before the choosen element
-            while (j >= 0 && element < nums[j]) { // index must be >=0 and element should be smaller than the value at
-                                                  // that index in sorted portion
-                nums[j + 1] = nums[j];
-                j--; // moving backward
+            // j index must be valid (j>=0) and element should be smaller than the value at that index in sorted portion
+            while (j >= 0 && element < nums[j]) {
+                nums[j + 1] = nums[j]; // shift the element from sorted portion to one step forward
+                j--; // moving backward in sorted portion
             }
 
             // placing the element when condition goes wrong i.e., all indices are checked
@@ -55,6 +61,43 @@ public class Sorting {
         }
         System.err.print("Sorted Numbers by insertion sort: ");
         printArray(nums);
+        int[] arr = {11,2,4,6,1,4,8,10};
+        shellSort(arr);
+        System.out.print("Shell Sort: ");
+        printArray(arr);
+    }
+
+    /*  Shell Sort
+     * Sorting based on comparisons with elements that are specific distance away - gap
+     * Time Complexity: Typically ranges from 𝑂(𝑛 log 𝑛) to 𝑂(𝑛²) depending on the gap sequence used, but generally * performs much better than Insertion Sort for larger arrays.
+     */
+    
+    public static void shellSort(int[] array) {
+        if (array.length <= 1) { // just in case invalid array or array with single element arrives
+            return;
+        } else {
+            int gap, swap;
+            gap = array.length / 2; // there are more methods of calculating gap as well
+            while (gap != 0) {
+                for (int i = gap; i < array.length; i++) { // Start from index (i) = gap to end of array
+                    /* Get the element that is gap distance before the upper element
+                     * If swapping occures, we need to check for swap gap distance backwards too
+                     * That's why j is decremented gap times
+                    */
+                    for (int j = i - gap; j >= 0; j -= gap) {
+                        if (array[j] > array[j + gap]) { // compare array[j] with gap distance forward
+                            // swap
+                            swap = array[j];
+                            array[j] = array[j + gap];
+                            array[j + gap] = swap;
+                        } else {
+                            break; // if swapping not occures, not need to update j and check backwards
+                        }
+                    }
+                }
+                gap /= 2; // update the gap
+            }
+        }
     }
 
     public static void printArray(int[] arr) {
