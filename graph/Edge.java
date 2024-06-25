7cfd1828950566c6827d362e77c7fd4007463042
yousuf-git <hackerbest052@gmail.com>
2024-06-25 13:38:48 +0500

    Updated for weighted graphs

--- graph/Edge.java
+++ graph/Edge.java
@@ -1,11 +1,46 @@
+/* Edge class for graph that contains the information of source and destination which are genric type
+ * If graph is weighted, edge has wt attribute that will be utilized, otherwise it will null
+*/
+
 package graph;
-// Edge class for graph that contains the information of source and destination which are genric type
-public class Edge<T> {
+
+public class Edge<T> implements Comparable<Edge<T>> {
     T src;
     T dest;
+    Integer wt;
 
-    Edge(T src, T dest) {
+    /**
+     * Construtor for unweighted edge
+     * 
+     * @param src  => source of edge
+     * @param dest => destination of edge
+     */
+    public Edge(T src, T dest) {
         this.src = src;
         this.dest = dest;
+        this.wt = null;
+    }
+
+    /**
+     * Construtor for weighted edge
+     * 
+     * @param src
+     * @param dest
+     * @param wt   => weight / cost of edge
+     */
+    public Edge(T src, T dest, int wt) {
+        this.src = src;
+        this.dest = dest;
+        this.wt = wt;
+    }
+
+    // To Compare any 2 edges based on their weights, here less weight edge will be
+    // considered as smaller
+    @Override
+    public int compareTo(Edge<T> edge) {
+        return this.wt - edge.wt;
+        // if it returns +ve value, it means current edge is greater than provided one
+        // if it returns -ve value, it means current edge is smaller than provided one
+        // if it returns zero, it means current edge is equal to provided one
     }
 }
