6d2765ad4dd6e234c96ddececcc9e277957f863e
yousuf-git <hackerbest052@gmail.com>
2024-06-25 13:38:14 +0500

    Added setter and getter

--- 14-linkedList/hashmap/HmEntry.java
+++ 14-linkedList/hashmap/HmEntry.java
@@ -1,14 +1,28 @@
+/*----------HashMap Entry class that contains a HashMap node that has 2 attributes : key and value---------*/
+
 package hashmap;
 
 public class HmEntry<K, V> {
-    HmNode<K, V> node;
+    HmNode<K, V> node; // see HmNode.java for its implementation
+
     HmEntry(HmNode<K, V> node) {
         this.node = node;
     }
+
     public K getKey() {
         return node.key;
     }
+
     public V getValue() {
         return node.value;
     }
+
+    public V setValue(V value) {
+        V currValue = node.value; // get the old value
+        node.value = value; // update the value
+        return currValue; // return the old value
+    }
 }
+
+// Reference to official doc of Map.Entry class
+// https://docs.oracle.com/javase/8/docs/api/java/util/Map.Entry.html
--- 14-linkedList/hashmap/HmNode.java
+++ 14-linkedList/hashmap/HmNode.java
@@ -1,19 +1,19 @@
 /* HashMap Node structure
  * Hasmap node has 2 components in it
- * 1. key --> Type K
- * 2. value --> Type V
- * 
- * 
-*/ 
-
+ * 1. key --> Type K (generic)
+ * 2. value --> Type V (generic)
+*/
 
 package hashmap;
 
-
-
 public class HmNode<K, V> {
-    K key;
-    V value;
+    public K key;
+    public V value;
+
+    /** Constructor
+     * @param key
+     * @param value
+     */
     public HmNode(K key, V value) {
         this.key = key;
         this.value = value;
