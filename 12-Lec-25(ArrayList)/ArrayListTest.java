bb60a25e8ce1d924d96ff729241e4e118e7efce1
yousuf-git <hackerbest052@gmail.com>
2024-07-20 21:00:53 +0500

    Optimized: addAll() and retainAll() Fixed: Mistake in clear() and add()

--- 12-Lec-25(ArrayList)/ArrayListTest.java
+++ 12-Lec-25(ArrayList)/ArrayListTest.java
@@ -1,25 +1,31 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayListTest {
     public static void main(String[] args) {
+        System.out.println("\nCreating List........");
         HArrayList<Integer> list = new HArrayList<>();
 
+        System.out.println("\nCheck if List is Empty\n");
         if (list.isEmpty()) {
-            System.out.println("List is Empty !");
+            System.out.println("Yes, List is Empty !");
         } else {
-            System.out.println("List is not empty");
+            System.out.println("No, List is not empty");
         }
 
+        System.out.println("\nAdding 14 elements in list\n");
         for (int i = 1; i < 15; i++) {
-            if (list.add(i*2)) {
-                System.out.println(i*2 + " Added");
+            if (list.add(i * 2)) {
+                System.out.println(i * 2 + " Added");
             } else {
                 System.out.println("Error at " + i);
             }
         }
 
+        System.out.println("\nChecking size of list\n");
         System.out.println("Size of List : " + list.size());
 
+        System.out.println("\nPrinting Elements by get() method\n");
         System.out.print("Elements in list: ");
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i) + " ");
@@ -27,14 +33,16 @@ public class ArrayListTest {
         System.out.println();
 
         int element = 69;
+        System.out.println("\nChecking if list contains " + element + "\n");
         if (list.contains(element)) {
             System.out.println(element + "found ");
         } else {
             System.out.println(element + " not found");
         }
-        element = 12;
-        int idx = list.indexOf(element);
 
+        element = 12;
+        System.out.println("\nGetting index of " + element + "\n");
+        int idx = list.indexOf(element);
         if (idx != -1) {
             System.out.println(element + " is at index " + idx);
         } else {
@@ -48,38 +56,41 @@ public class ArrayListTest {
         
         Q: If list is integer type and we call remove(10) which one will be called?
         A: remove(int index) will be called bcz it is most specific applicable method for method overloading resolution
-
-        So, if I want to remove an element 10 itself from the list, I'll call remove((Integer) 10) - cast
-
-        */ 
+        
+        So, if I want to remove an element 10 itself from the list, I'll call remove((Integer) 10) - by casting
+        
+        */
 
         // Testing: remove(int index) - returns element / null
+        System.out.println("\nTesting remove(int index)\n");
         element = 10;
         Integer removedElement = list.remove(element); // here element will be as index
         if (removedElement != null) {
             System.out.println(removedElement + " removed !");
         } else {
-            System.out.println(element + " not found in list !");
+            System.out.println("Invalid Index!");
         }
 
         // Testing: remove(E obj) - returns true / false
+        System.out.println("\nTesting remove(E obj)\n");
         if (list.remove((Integer) element)) {
             System.out.println(element + " removed !");
         } else {
             System.out.println(element + " not found in list");
         }
-        System.out.print("Elements in list: ");
-        for (int i = 0; i < list.size(); i++) {
-            System.out.print(list.get(i) + " ");
-        }
-        System.out.println();
+
+        System.out.println("\nPrinting elements of list by display()\n");
+        list.display();
         System.out.println("Updated Size of List : " + list.size());
 
+
+        System.out.println("\nTesting toArray()\n");
         Object[] copy = list.toArray();
-        System.out.print("Copy List: ");
+        System.out.print("Copied Array of List: ");
         for (Object val : copy) {
             System.out.print(val + " ");
         }
+        System.out.println();
         // If we want to convert this array to integer type
         // Integer[] copyList = new Integer[copy.length];
 
@@ -90,21 +101,27 @@ public class ArrayListTest {
         // for (int i = 0; i < copyList.length; i++) {
         //     System.out.print(copyList[i] + " ");
         // }
+        // System.out.println();
 
+        System.out.println("\nTesting removeAll(Collection<?> c)\n");
+        ArrayList<Integer> listToRemove = new ArrayList<>();
+        listToRemove.add(2);
+        listToRemove.add(4);
+        listToRemove.add(6);
+        listToRemove.add(8);
+        listToRemove.add(11);
+        // ArrayList<String> listToRemove = new ArrayList<>();
+        // listToRemove.add("2");
+        // listToRemove.add("4");
+        // listToRemove.add("6");
+        // listToRemove.add("8");
+        // listToRemove.add("11");
+
+        System.out.print("List of elements that are to be removed: ");
+        for (Object val : listToRemove) {
+            System.out.print(val + " ");
+        }
         System.out.println();
-
-        // ArrayList<Integer> listToRemove = new ArrayList<>();
-        // listToRemove.add(2);
-        // listToRemove.add(4);
-        // listToRemove.add(6);
-        // listToRemove.add(8);
-        // listToRemove.add(11);
-        ArrayList<String> listToRemove = new ArrayList<>();
-        listToRemove.add("2");
-        listToRemove.add("4");
-        listToRemove.add("6");
-        listToRemove.add("8");
-        listToRemove.add("11");
 
         list.removeAll(listToRemove);
 
@@ -112,12 +129,65 @@ public class ArrayListTest {
         list.display();
         System.out.println("Updated Size : " + list.size());
 
+        System.out.println("\nChecking if copied array is affected by original list\n");
         System.out.print("Copy List: ");
         for (Object val : copy) {
             System.out.print(val + " ");
         }
         System.out.println();
 
+        System.out.println("\nTesting addAll(Collection<? extends E> c)\n");
+        list.addAll(listToRemove); // adding the same elements again that were removed
+        System.out.print("Updated ");
+        list.display();
+
+        System.out.println("\nTesting retainAll(Collection<?> c)\n");
+        listToRemove.add(16);
+        System.out.print("List of elements that are to be retained: ");
+        for (Object val : listToRemove) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+        if (list.retainAll(listToRemove)) { // only these elements should be retained
+            System.out.print("Updated ");
+            list.display();
+        } else {
+            System.out.println("No Change in list");
+        }
+
+        System.out.println("\nTesting addAll(int index, Collection<? extends E> c)\n");
+
+        System.out.print("Original ");
+        list.display();
+        System.out.print("List of elements that are to be added: ");
+        for (Object val : listToRemove) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+
+        list.addAll(4, listToRemove);
+        System.out.print("Updated ");
+        list.display();
+
+        System.out.println("\nTesting copy()\n");
+        HArrayList<Integer> duplicate = list.copy();
+        System.err.print("Duplicate ");
+        duplicate.display();
+        duplicate.clear();
+        System.err.print("Updated Duplicate ");
+        duplicate.display();
+        System.out.println("Last Idx of 2: " + list.lastIndexOf(2));
+
+        List<Integer> subList = list.subList(1, list.size());
+        if (subList == null) {
+            System.out.println("List is empty or invalid range !");
+        } else {
+            System.out.print("Sub List: ");
+            for (Integer integer : subList) {
+                System.out.print(integer + " ");
+            }
+            System.out.println();
+        }
     }
 }
 /*
@@ -133,6 +203,5 @@ ____________________________
 if(size = length) -> rehashing
 
 if size = 7 means array is filled, now rehashing will be done (new list will be created)
-
 
 */
--- 12-Lec-25(ArrayList)/HArrayList.java
+++ 12-Lec-25(ArrayList)/HArrayList.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 // import java.util.ArrayList;
+import java.util.Set;
 
 /**
 _________This class has the basic implementation of ArrayList from scratch_________
@@ -21,7 +23,7 @@ public class HArrayList<E> {
      * @construcor
      */
     @SuppressWarnings("unchecked")
-    HArrayList() {
+    public HArrayList() {
         size = 0;
         // list = new E[2]; // generic types are erased at runtime for backward compatibility reasons
         list = (E[]) new Object[10]; // create object and then type cast it
@@ -54,8 +56,26 @@ public class HArrayList<E> {
         return copy;
     }
 
-    public <T> T[] toArray(T[] a) {
-        throw new UnsupportedOperationException("Unimplemented method 'toArray'");
+    /*----To put elements of list into given array------ */
+    public <T> E[] toArray(E[] array) {
+        E[] arr;
+        // Validate If given array is enough to accomodate all elements or not
+        if (array.length >= this.size) {
+            arr = array;
+        } else {
+            @SuppressWarnings("unchecked")
+            E[] newArray = (E[]) new Object[size];
+            arr = newArray;
+        }
+        // Copy all elements in same order
+        for (int i = 0; i < size; i++) {
+            arr[i] = list[i];
+        }
+        // If length of given array is larger than list, nullify remaining elements
+        for (int i = size; i < arr.length; i++) {
+            arr[i] = null;
+        }
+        return arr;
     }
 
     // Time Complexity
@@ -71,7 +91,7 @@ public class HArrayList<E> {
         return true;
     }
 
-    // Resizing - O(n)
+    // Resizing Time Complexity - O(n)
     private boolean resize() {
         try {
             // Recreate the list - with length 50% more of original one
@@ -82,7 +102,7 @@ public class HArrayList<E> {
                 newList[i] = list[i];
             }
             list = newList; // reference the old list to new list now
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
             return false; // In case if resizing cannot be done
         }
@@ -96,15 +116,15 @@ public class HArrayList<E> {
     */
     public boolean remove(Object obj) {
         // First check if obj is present in the list or not
-        int index = indexOf(obj); // get the index of object and then remove it using remove(idx)
+        int index = indexOf(obj); // get the index of object
         if (index != -1) {
-            remove(index);
+            remove(index); // remove it using remove(int idx)
             return true;
         } else {
             return false;
         }
     }
-    
+
     // Time Complexity: O(n)       Shifting n elements in worse case
     public E remove(int index) {
         if (index < 0 || index >= size) {
@@ -116,7 +136,7 @@ public class HArrayList<E> {
             list[i] = list[i + 1];
         }
         size--; // decrement the size
-        return element;
+        return element; // return the old elememnt present at that index
     }
 
     // Time Complexity: O(m * n) where m is length of collection and n is size of list
@@ -133,48 +153,166 @@ public class HArrayList<E> {
     // Time Complexity: O(m) where m is the number of elements in the collection
     public boolean addAll(Collection<? extends E> collection) {
         for (E element : collection) {
-            if(add(element)) {
+            if (!add(element)) { // if element cannot be added (resizing issue)
                 return false;
             }
         }
         return true;
     }
 
+    /* -------- To Add a list at specific position -----------
+     *  - The previous Elements will be shifted right
+     *  Time Complexity: O(m*n)     where m is elements in the collection
+     *  Can be optimized by shifting one time only
+     *  By calling add(idx) for each element, shifting is done each time
+     *  In worse case there will be shifting of n elements m times
+     */
+    // public boolean addAll(int index, Collection<? extends E> c) {
+    //     // Validation
+    //     if (c == null || c.size() == 0) {
+    //         return false;
+    //     }
+    //     if (index < 0 || index >= size) {
+    //         return false;
+    //     }
+
+    //     for (E element : c) {
+    //         if (!add(index, element)) {
+    //             System.out.println("Some error occured while resizing");
+    //             return false;
+    //         }
+    //         index++;
+    //     }
+    //     return true;
+    // }
+    /* Optimized addAll()
+     * Time Complexity Analysis:
+     *  - Shifting -> O(n)      n = this.size()
+     *  - Placing -> O(m)       m = c.size()
+     *  - Total  = O(n) + O(m)
+     * Optimized from O(m*n) to O(m+n)
+     * 
+    */
     public boolean addAll(int index, Collection<? extends E> c) {
-        throw new UnsupportedOperationException("Unimplemented method 'addAll'");
+        // Validation
+        if (c == null || c.size() == 0) {
+            return false;
+        }
+        if (index < 0 || index >= size) {
+            return false;
+        }
+        int s = c.size();
+        // Resize the lise untill all elements can be added
+        while (s + size > list.length) {
+            if (!resize()) {
+                return false;
+            }
+        }
+        // Shift the prrvious elements at once
+        for (int i = size-1; i >= index; i--) {
+            list[i + s] = list[i];
+        }
+        // Place the elements in the collection
+        int idx = index;
+        for (E e : c) {
+            list[idx] = e;
+            idx++;
+        }
+        size += s;
+
+        return true;
     }
 
     // Time Complexity: O(n * m)
     public boolean removeAll(Collection<?> collection) {
         boolean isChanged = false;
         for (Object object : collection) {
-            if(this.remove(object)) {
+            if (this.remove(object)) {
                 isChanged = true;
             }
         }
         return isChanged;
     }
 
+    /*-------Retain only these elements of collection - remove others from list -------
+     * Time Complexity: O(n * (m+n))
+     */
+    // public boolean retainAll(Collection<?> collection) {
+    //     // If empty or null collection is passed, remove all elements in the list
+    //     if (collection == null || collection.size() == 0) {
+    //         this.clear();
+    //         return true;
+    //     }
+    //     boolean isModified = false;
+    //     // Iterate over all the elements in the list
+    //     for (int i = 0; i < size; i++) {
+    //         System.out.print(i + " ");
+    //         E element = list[i];
+    //         System.out.println("Grabbed Element: " + element);
+    //         // If the element from the list is not present in the collection - remove it
+    //         if (!collection.contains(element)) {
+    //             if (this.remove(element)) {
+    //                 System.out.println("Removed");
+    //                 i -= 1;
+    //                 isModified = true; // if list is modified
+    //             }
+    //         }
+    //     }
+    //     return isModified;
+    // }
+    /*-----Optimized retainAll------------
+     *  - Store elements of collection in a set -> O(m)
+     *  - Iterate over the old list -> O(n)
+     *  - Search in set -> O(1)
+     *  Total Time Complexity -> O(m+n)
+     */
     public boolean retainAll(Collection<?> collection) {
+        // If empty or null collection is passed, remove all elements in the list
+        if (collection == null || collection.size() == 0) {
+            this.clear();
+            return true;
+        }
+        Set<?> set = new HashSet<>(collection); // O(m)
         boolean isModified = false;
+        int newIdx = 0;
+        int newSize = 0;
         // Iterate over all the elements in the list
-        for (E element : list) {
-            // If the element from the list is not present in the collection - remove it
-            if (!collection.contains(element)) {
-                if(this.remove(element)) {
-                    isModified = true; // if list is modified
-                }
+        for (int i = 0; i < size; i++) {
+            E element = list[i];
+            // If the element from the list is not present in the set - remove it
+            if (set.contains(element)) {
+                // retain it
+                list[newIdx++] = element;
+                newSize += 1;
+            } else {
+                list[i] = null;
+                isModified = true;
             }
         }
+        this.size = newSize;
+
         return isModified;
     }
 
     /*-------Remove all the elements from the list------- */
     // Time Complexity: O(n)
     public void clear() {
-        for (E element : list) {
-            this.remove(element);
+        // Mistake
+        // for (int i = 0; i < size; i++) {
+        //     this.remove(list[i]);
+        // }
+        // Method-1: Not Optimized -> O(n^2)
+        // int idx = 0;
+        // while (!this.isEmpty()) {
+        //     this.remove(idx);
+        // }
+        // Method-2: Optimized -> O(n)
+        int idx = size - 1;
+        while (!this.isEmpty()) {
+            remove(idx);
+            idx = size - 1;
         }
+        System.out.println("List Cleared !");
     }
 
     // Time Complexity: O(1)
@@ -185,31 +323,45 @@ public class HArrayList<E> {
         return list[index];
     }
 
-    // Time Complexity: O(1)
+    /**
+     * Replaces the element at the specified position in this list with the specified element.
+     *
+     * @param index the index of the element to replace
+     * @param element the element to be stored at the specified position
+     * @return the element previously at the specified position
+     * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index >= size)
+     * Time Complexity: O(1)
+     */
     public E set(int index, E element) {
         if (index < 0 || index >= size) {
-            return null;
+            throw new IndexOutOfBoundsException();
         }
         E oldElement = list[index];
         list[index] = element;
         return oldElement;
     }
 
+    /* 
+     * Shifts the old element at given index and all subsequents to right
+     * Time Complexity: O(n) in worse case we need to move all elements to right
+     */
     public boolean add(int index, E element) {
         // Validation of index
         if (index < 0 || index >= size) {
             return false;
         }
         // Shift all elements from given index to size, one step forward
-        for (int i = index; i < size; i++) {
-            if (index == size) {
-                if(!resize()) {
+        for (int i = size - 1; i >= index; i--) {
+            if (i == list.length - 1) {
+                if (!resize()) {
+                    System.out.println("Resizing issue");
                     return false;
                 }
             }
-            list[i+1] = list[i];
+            list[i + 1] = list[i];
         }
         list[index] = element;
+        size++;
         return true;
     }
 
@@ -217,6 +369,7 @@ public class HArrayList<E> {
     public int indexOf(Object o) {
         for (int i = 0; i < size; i++) {
             if (list[i].equals(o)) {
+                // return the very first index
                 return i;
             }
         }
@@ -234,19 +387,43 @@ public class HArrayList<E> {
         return idx;
     }
 
-    public List<E> subList(int fromIndex, int toIndex) {
+    /* Returns a list in given range, last index is not included
+     * Time Complexity: O(n) for worse case, subList = list
+     */
+    public List<E> subList(int fromIdx, int toIdx) {
+        // Validation
+        if (fromIdx > toIdx || fromIdx < 0 || fromIdx >= size || toIdx < 0 || toIdx > size || isEmpty()) {
+            return null;
+        }
         List<E> subList = new ArrayList<>();
-        for (int i = fromIndex; i < toIndex; i++) {
-            subList.add(list[i]);
+        int idx = fromIdx;
+        subList.add(list[fromIdx]);
+        while (idx < toIdx - 1) {
+            idx++;
+            subList.add(list[idx]);
         }
         return subList;
     }
 
+    // Time Complexity: O(n)
     public void display() {
+        if (this.isEmpty()) {
+            System.out.println("List: []");
+            return;
+        }
         System.out.print("List: ");
         for (int i = 0; i < size; i++) {
             System.out.print(list[i] + " ");
         }
         System.out.println();
+    }
+
+    // Time Complexity: O(n)
+    public HArrayList<E> copy() {
+        HArrayList<E> copy = new HArrayList<>();
+        for (int i = 0; i < size; i++) {
+            copy.add(list[i]);
+        }
+        return copy;
     }
 }
